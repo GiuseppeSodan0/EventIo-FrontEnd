2923f2f0129a51f5d8e3a6c39582ba46ae7ed4d4
Daniele Cavarra <danielecavarra97@gmail.com>
2026-04-21 12:58:20 +0200

    admin panel

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Admin } from './pages/admin/admin';
+import { AdminTickets } from './pages/admin-tickets/admin-tickets/admin-tickets';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'admin', component: Admin },
+  { path: 'admin-tickets', component: AdminTickets },
+  { path: '', redirectTo: 'admin', pathMatch: 'full' }
+];
