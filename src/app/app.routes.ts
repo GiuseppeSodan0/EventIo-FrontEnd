a857361f5614bfce6db0266b946a1d3db46bb3b9
Lorenzo <capozzolilorenzo1995@gmail.com>
2026-04-21 12:54:55 +0200

    aggiornamento codice

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home-component/home-component';
 import { EventComponent } from './event-component/event-component';
+import { PaymentComponent } from './payment-component/payment-component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'home', component: HomeComponent },
     { path: 'events', component: EventComponent },
+    { path: 'payments', component: PaymentComponent },
     { path: '**', redirectTo: 'home' },
 ];
