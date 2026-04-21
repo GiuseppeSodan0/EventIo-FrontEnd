60a57a7d381cf5959980407f17aea0a00f93f919
Daniele Cavarra <danielecavarra97@gmail.com>
2026-04-21 15:06:34 +0200

    fix conflict

--- src/app/Service/user-service.ts
+++ src/app/Service/user-service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from "@angular/common/http";
+/*import { HttpClient } from "@angular/common/http";
 import { UserDto } from "./UserDto";
 
 export class userService extends AbstractService<UserDto>{
