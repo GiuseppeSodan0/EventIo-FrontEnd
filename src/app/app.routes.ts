704009b1bc41d87ec51eaa8864617632975ea87e
giulioborriello <75851725+giulioborriello@users.noreply.github.com>
2026-04-21 10:52:07 +0200

    aggiunto redirect su app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,8 @@ import { HomeComponent } from './home-component/home-component';
 import { EventComponent } from './event-component/event-component';
 
 export const routes: Routes = [
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'home', component: HomeComponent },
     { path: 'events', component: EventComponent },
+    { path: '**', redirectTo: 'home' },
 ];
