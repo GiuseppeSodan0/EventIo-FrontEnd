bf7e0be1c817edf997e76c2f3b43bd551d3c3b02
giulioborriello <75851725+giulioborriello@users.noreply.github.com>
2026-04-21 10:38:09 +0200

    aggiunto HomeComponent e EventComponent ad app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home-component/home-component';
+import { EventComponent } from './event-component/event-component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'home', component: HomeComponent },
+    { path: 'events', component: EventComponent },
+];
