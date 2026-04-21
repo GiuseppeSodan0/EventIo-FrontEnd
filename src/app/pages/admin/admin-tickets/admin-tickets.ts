b054b754b41538550d0942f6240fc62dc5efc1b4
Daniele Cavarra <danielecavarra97@gmail.com>
2026-04-21 11:15:40 +0200

    aggiunto pages\admin e admin-tickets

---
+++ src/app/pages/admin/admin-tickets/admin-tickets.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-tickets',
+  imports: [],
+  templateUrl: './admin-tickets.html',
+  styleUrl: './admin-tickets.css',
+})
+export class AdminTickets {}
