e50a759d000cde122fe2f5581a72dc59d25f80fe
giulioborriello <75851725+giulioborriello@users.noreply.github.com>
2026-04-22 10:38:24 +0200

    aggiunto il login alla navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home-component/home-component';
 import { EventComponent } from './event-component/event-component';
 import { PaymentComponent } from './payment-component/payment-component';
+import { LoginComponent } from './login-component/login-component';
 
 
 export const routes: Routes = [
@@ -9,5 +10,6 @@ export const routes: Routes = [
     { path: 'home', component: HomeComponent },
     { path: 'events', component: EventComponent },
     { path: 'payments', component: PaymentComponent },
+    { path: 'login', component: LoginComponent },
     { path: '**', redirectTo: 'home' },
 ];
