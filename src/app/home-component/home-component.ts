07b9724f22e82a3afd647d3a33c9b5f434c6988e
giulioborriello <75851725+giulioborriello@users.noreply.github.com>
2026-04-22 09:40:08 +0200

    risolto il problema del doppio click sulla home tramite signal

--- src/app/home-component/home-component.ts
+++ src/app/home-component/home-component.ts
@@ -1,4 +1,4 @@
-import { Component, NgZone } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { EventDto } from '../Dto/EventDto';
 import { EventService } from '../Service/event-service';
 import { CommonModule } from '@angular/common';
@@ -11,64 +11,23 @@ import { CommonModule } from '@angular/common';
   styleUrl: './home-component.css',
 })
 export class HomeComponent {
-  mostRenumerativeEvents: EventDto[] = [];
+  private readonly eventService = inject(EventService);
 
-  constructor(private eventService: EventService, private ngZone: NgZone) {}
-
-  currentIndex = 0;
-  intervalId: any;
+  readonly mostRenumerativeEvents = signal<EventDto[]>([]);
 
   ngOnInit(): void {
     this.getMostRenumerativeEvents();
   }
 
   getMostRenumerativeEvents(): void {
-  this.eventService.findTop5MostRemunerative().subscribe({
-    next: (events: EventDto[]) => {
-      this.mostRenumerativeEvents = events;
-      this.startAutoSlide(); // 👈 AVVIA SLIDER
-    },
-    error: (error: unknown) => {
-      console.error('Error fetching most remunerative events:', error);
-    },
-  });
-}
-
-startAutoSlide() {
-  this.stopAutoSlide();
-
-  this.intervalId = setInterval(() => {
-
-    this.ngZone.run(() => {  // 👈 QUESTO RISOLVE TUTTO
-      this.next();
+    this.eventService.findTop5MostRemunerative().subscribe({
+      next: (events: EventDto[]) => {
+        this.mostRenumerativeEvents.set(events);
+      },
+      error: (error: unknown) => {
+        console.error('Error fetching most remunerative events:', error);
+      },
     });
-
-  }, 3000);
-}
-
-stopAutoSlide() {
-  if (this.intervalId) {
-    clearInterval(this.intervalId);
   }
-}
-
-next() {
-  if (!this.mostRenumerativeEvents.length) return;
-
-  this.currentIndex =
-    (this.currentIndex + 1) % this.mostRenumerativeEvents.length;
-
-  this.startAutoSlide(); // 🔥 reset timer
-}
-
-prev() {
-  if (!this.mostRenumerativeEvents.length) return;
-
-  this.currentIndex =
-    (this.currentIndex - 1 + this.mostRenumerativeEvents.length) %
-    this.mostRenumerativeEvents.length;
-
-  this.startAutoSlide(); // 🔥 reset timer
-}
 
 }
