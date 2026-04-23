385d7623a4f826c7734effa92c678d243f183c42
gotuax <giuseppe.sodano.1996@gmail.com>
2026-04-23 14:11:11 +0200

    add/fix

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,22 +1,22 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home-component/home-component';
 import { EventComponent } from './event-component/event-component';
-import { PaymentComponent } from './payment-component/payment-component';
 import { LoginComponent } from './login-component/login-component';
-import { UserAreaComponent } from './user-area/user-area';
-import { AdminComponent} from './admin-component/admin-component';
-import { AdminTicketsComponent } from './admin-tickets-component/admin-tickets-component';
 import { RegisterComponent } from '../register-component/register-component';
+import { UserAreaComponent } from './user-area-component/user-area-component';
+import { PaymentComponent } from './payment-component/payment-component';
+import { AdminComponent } from './admin-component/admin-component';
+import { AdminTicketsComponent } from './admin-tickets-component/admin-tickets-component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'home', component: HomeComponent },
-    {path: 'register',component: RegisterComponent},
     { path: 'events', component: EventComponent },
-    { path: 'payments', component: PaymentComponent },
     { path: 'login', component: LoginComponent },
+    { path: 'register', component: RegisterComponent },
     { path: 'user-area', component: UserAreaComponent },
+    { path: 'payments', component: PaymentComponent },
     { path: 'admin', component: AdminComponent },
     { path: 'admin-tickets', component: AdminTicketsComponent },
-    { path: '**', redirectTo: 'home' },
+    { path: '**', redirectTo: 'home' }
 ];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,15 +1,33 @@
 import { Component, signal } from '@angular/core';
-import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
-import { AuthService } from './Service/auth-service';
+import { RouterOutlet, RouterLink, RouterLinkActive, Router, NavigationEnd } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { filter } from 'rxjs/internal/operators/filter';
+import { AuthService } from './Service/auth-service';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, RouterLink, RouterLinkActive, CommonModule],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
+  standalone: true
 })
-export class App {
+export class App  {
   protected readonly title = signal('EventIo-FrontEnd');
 
-  constructor(public auth: AuthService) {}
+  isUserArea = false;
+
+  constructor(private router: Router, private authService: AuthService) {}
+
+  ngOnInit(): void {
+    this.router.events.pipe(
+      filter(e => e instanceof NavigationEnd)
+    ).subscribe((e: NavigationEnd) => {
+      const url = e.urlAfterRedirects;
+      this.isUserArea = url.startsWith('/user-area') || url.startsWith('/payments');
+    });
+  }
+
+  logout(): void {
+    this.authService.logout();
+    this.router.navigate(['/login']);
+  }
 }
