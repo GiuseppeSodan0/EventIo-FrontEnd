d32ab08345b85bc6da8042a5ac12fb659ac153c8
gotuax <giuseppe.sodano.1996@gmail.com>
2026-04-23 14:11:39 +0200

    fix/update2

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,32 +2,35 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet, RouterLink, RouterLinkActive, Router, NavigationEnd } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { filter } from 'rxjs/internal/operators/filter';
-import { AuthService } from './Service/auth-service';
+import { NavbarUserComponent } from "./components/navbar-user/navbar-user";
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, RouterLinkActive, CommonModule],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, CommonModule, NavbarUserComponent],
   templateUrl: './app.html',
-  styleUrl: './app.css',
-  standalone: true
+  styleUrl: './app.css'
 })
 export class App  {
   protected readonly title = signal('EventIo-FrontEnd');
 
-  isUserArea = false;
+    isUserArea = false;
 
-  constructor(private router: Router, private authService: AuthService) {}
+  constructor(private router: Router) {}
 
   ngOnInit(): void {
+    // Controlla l'URL iniziale
+    this.checkRoute(this.router.url);
+
+    // Controlla ad ogni cambio di rotta
     this.router.events.pipe(
       filter(e => e instanceof NavigationEnd)
     ).subscribe((e: NavigationEnd) => {
-      const url = e.urlAfterRedirects;
-      this.isUserArea = url.startsWith('/user-area') || url.startsWith('/payments');
+      this.checkRoute(e.urlAfterRedirects);
     });
   }
 
-  logout(): void {
-    this.authService.logout();
-    this.router.navigate(['/login']);
+  private checkRoute(url: string): void {
+    // Mostra navbar utente su queste pagine
+    this.isUserArea = url.startsWith('/user-area') || url.startsWith('/payments');
   }
+
 }
