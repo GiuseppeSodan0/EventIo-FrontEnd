443b3ed948ffa4b67f2b6a190f0ea9bc5d288c14
Daniele Cavarra <danielecavarra97@gmail.com>
2026-04-24 13:34:39 +0200

    save

--- src/app/admin-component/admin-component.spec.ts
+++ src/app/admin-component/admin-component.spec.ts
@@ -1,4 +1,4 @@
-/*import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AdminComponent } from './admin-component';
 
 describe('AdminComponent', () => {
@@ -18,4 +18,4 @@ describe('AdminComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
-});*/
+});
