c2fd2735a590946277ba9c56bab969c29648f0da
giulioborriello <75851725+giulioborriello@users.noreply.github.com>
2026-04-27 12:05:01 +0200

    eliminata search event component

--- src/app/addOn/search-event-component/search-event-component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-search-event-component',
-  imports: [],
-  templateUrl: './search-event-component.html',
-  styleUrl: './search-event-component.css',
-})
-export class SearchEventComponent {}
